39e3d7e5fee54d7c0068efe583c0fd0ba07d042f
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2023-11-01 21:25:12 +0200

    trails

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,73 @@
-use crate::triangle::Triangle;
-
 mod triangle;
 
-#[derive(Debug)]
-struct Person {
-    name: String,
-    surname: String,
-    age: i32,
-    balance: f64,
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
 }
-
 
 fn main() {
-    // let tr1 = Triangle {
-    //     cat1: 6.0,
-    //     cat2: 8.0,
-    // };
+    let number_list = vec![34, 50, 25, 100, 65];
+    println!("The largest number is {}", largest(&number_list));
 
-    // let hyp = tr1.find_hyp();
-    // println!("{hyp}");
-    // let area = tr1.find_area();
-    // println!("{area}");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    let isc_tr = Triangle::create_isc(5.0);
-    println!("{:?}", isc_tr);
-
-    let hyp = isc_tr.find_hyp();
-
-    let area = isc_tr.find_area();
-
-    println!("{hyp} {area}")
+    println!("1 new tweet: {}", tweet.summarize());
 }
+
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
